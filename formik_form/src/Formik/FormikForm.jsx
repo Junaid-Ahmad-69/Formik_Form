e003f72606a2b66a391232a0e1e6010744e1a449
Junaid Sandhu <junaidsandhu21@gmail.com>
2023-02-01 18:48:49 +0500

    Implement Formik impure Component Approach

--- formik_form/src/Formik/FormikForm.jsx
+++ formik_form/src/Formik/FormikForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useFormik} from 'formik'
+import {Formik, Field, Form, ErrorMessage} from 'formik'
 import {SignUp} from "../Schema"
 import styles from "./FormikForm.module.css"
 import bannerImage from "../assets/sign.jpg"
@@ -14,14 +15,6 @@ const initialValues = {
 
 const FormikForm = () => {
 
-    const {values, errors, handleBlur, handleChange, handleSubmit, touched} = useFormik({
-        initialValues: initialValues,
-        validationSchema: SignUp,
-        onSubmit: (values) => (e) => {
-            e.preventDefault();
-        }
-    })
-
     return (
         <>
             <div className={styles.wrapper}>
@@ -29,63 +22,75 @@ const FormikForm = () => {
                     <div className={styles.imgContainer}>
                         <img src={bannerImage} alt={bannerImage}/>
                     </div>
-                    <form className={styles.formControl} onSubmit={handleSubmit}>
-                        <section className={styles.formContent}>
-                            <h1>Create an Account</h1>
-                            <p>Please fill the data below to create your account</p>
-                        </section>
-                        <div className={styles.dataInput}>
-                            <label htmlFor="firstName">First Name</label>
-                            <input id="firstName" name="firstName" type="text" onChange={handleChange}
-                                   value={values.firstName} onBlur={handleBlur}/>
-                            {touched.firstName && errors.firstName ?
-                                <span className={styles.error}>{errors.firstName}</span> : null}
-                        </div>
-
-                        <div className={styles.dataInput}>
-                            <label htmlFor="lastName">Last Name</label>
-                            <input id="lastName" name="lastName" type="text" onChange={handleChange}
-                                   value={values.lastName} onBlur={handleBlur}/>
-                            {touched.lastName && errors.lastName ?
-                                <span className={styles.error}>{errors.lastName}</span> : null}
-
-                        </div>
-
-                        <div className={styles.dataInput}>
-                            <label htmlFor="email">Email</label>
-                            <input id="email" name="email" type="email" onChange={handleChange}
-                                   value={values.email} onBlur={handleBlur}/>
-                            {touched.email && errors.email ?
-                                <span className={styles.error}>{errors.email}</span> : null}
-
-                        </div>
 
 
-                        <div className={styles.dataInput}>
-                            <label htmlFor="password">Password</label>
-                            <input id="password" name="password" type="password" onChange={handleChange}
-                                   value={values.password} onBlur={handleBlur}/>
-                            {touched.password && errors.password ?
-                                <span className={styles.error}>{errors.password}</span> : null}
-                        </div>
+                    <Formik initialValues={initialValues} validationSchema={SignUp}
+                            onSubmit={(values, {setSubmitting}) => {
+                                setTimeout(() => {
+                                    alert(JSON.stringify(values, null, 2));
+                                    setSubmitting(false);
+                                }, 400);
+                            }}>
+                        {formik => {
+                            return (
+                                <form className={styles.formControl} onSubmit={formik.handleSubmit}>
+                                    <section className={styles.formContent}>
+                                        <h1>Create an Account</h1>
+                                        <p>Please fill the data below to create your account</p>
+                                    </section>
 
-                        <div className={styles.dataInput}>
-                            <label htmlFor="password">Confirm Password</label>
-                            <input id="confirmPassword" name="confirmPassword" type="password" onChange={handleChange}
-                                   value={values.confirmPassword} onBlur={handleBlur}/>
-                            {touched.confirmPassword && errors.confirmPassword ?
-                                <span className={styles.error}>{errors.confirmPassword}</span> : null}
-                        </div>
-                        <div className={styles.account}>
-                            <p className={styles.accountContent}>Already hava a account? <a href="#" className={styles.accountContent}>Sign in </a></p>
-                            <button className={styles.register} type="submit">Registered</button>
-                        </div>
-                    </form>
+                                    <div className={styles.dataInput}>
+                                        <label htmlFor="firstName">First Name</label>
+                                        <input id="firstName" type="text" {...formik.getFieldProps("firstName")}/>
+                                        {formik.touched.firstName && formik.errors.firstName ?
+                                            <span className={styles.error}>{formik.errors.firstName}</span> : null}
+                                    </div>
+
+                                    <div className={styles.dataInput}>
+                                        <label htmlFor="lastName">Last Name</label>
+                                        <input id="lastName" type="text" {...formik.getFieldProps("lastName")}/>
+                                        {formik.touched.lastName && formik.errors.lastName ?
+                                            <span className={styles.error}>{formik.errors.lastName}</span> : null}
+
+                                    </div>
+
+                                    <div className={styles.dataInput}>
+                                        <label htmlFor="email">Email</label>
+                                        <input id="email" type="email" {...formik.getFieldProps("email")}/>
+                                        {formik.touched.email && formik.errors.email ?
+                                            <span className={styles.error}>{formik.errors.email}</span> : null}
+
+                                    </div>
+
+                                    <div className={styles.dataInput}>
+                                        <label htmlFor="password">Password</label>
+                                        <input id="password" type="password" {...formik.getFieldProps("password")} />
+                                        {formik.touched.password && formik.errors.password ?
+                                            <span className={styles.error}>{formik.errors.password}</span> : null}
+                                    </div>
+
+                                    <div className={styles.dataInput}>
+                                        <label htmlFor="password">Confirm Password</label>
+                                        <input id="confirmPassword"
+                                               type="password" {...formik.getFieldProps("confirmPassword")}/>
+                                        {formik.touched.confirmPassword && formik.errors.confirmPassword ?
+                                            <span
+                                                className={styles.error}>{formik.errors.confirmPassword}</span> : null}
+                                    </div>
+                                    <div className={styles.account}>
+                                        <p className={styles.accountContent}>Already hava a account? <a href="#"
+                                                                                                        className={styles.accountContent}>Sign
+                                            in </a></p>
+                                        <button className={styles.register} type="submit">Registered</button>
+                                    </div>
+                                </form>
+                            );
+                        }}
+                    </Formik>
                 </div>
             </div>
-
         </>
-    )
+    );
 }
 
 export default FormikForm
